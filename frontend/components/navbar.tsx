bbc40c204b778cd1ecd1b0231da342459073e57b
Ander Alejandro Porath Ramirez <anderporath@MacBook-Air-de-Ander.local>
2026-03-20 13:35:10 -0300

    Logo Pagina

--- frontend/components/navbar.tsx
+++ frontend/components/navbar.tsx
@@ -43,7 +43,7 @@ export function Navbar() {
             alt="DescubrePY - Tu guia digital de Paraguay"
             width={360}
             height={108}
-            className="h-10 w-auto origin-left scale-[1.95]"
+            className="h-10 w-auto origin-left scale-[1.8]"
             priority
           />
         </Link>
